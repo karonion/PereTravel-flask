8e2081b66e961d89fabdd122ba2bc6693897ba90
karonion <EsyAtq2RghbWbLw>
2022-01-06 09:40:08 +0200

    Добавлены флэш сообщения после предоставления фидбэка и добавления нового поста. Сверстан пост подробно

--- app/views.py
+++ app/views.py
@@ -13,10 +13,10 @@ ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 def allowed_file(filename):
     return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS  # Функция проверки формата загружаемого фото
 
 
-@app.route('/images/<path:filename>')
+@app.route('/images/<path:filename>')  # Путь загужаемых файлов
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
@@ -25,33 +25,33 @@ def uploaded_file(filename):
 def upload():
     f = request.files.get('upload')
     extension = f.filename.split('.')[-1].lower()
-    if extension not in ['jpg', 'gif', 'png', 'jpeg']:
+    if extension not in ['jpg', 'gif', 'png', 'jpeg']:  # разрешённые форматы фото для загрузки
         return upload_fail(message='Тільки фото!')
     f.save(os.path.join(app.config['UPLOAD_FOLDER'], f.filename))
     url = url_for('uploaded_file', filename=f.filename)
     return upload_success(url, filename=f.filename)
 
 
-@app.route('/')
+@app.route('/')  # Отображение всех постов
 def main():
-    article_scope = Article.query.order_by(Article.date.desc())
+    article_scope = Article.query.order_by(Article.date.desc())  # Сортировка по дате создания
     return render_template('main.html', article_scope=article_scope)
 
 
-@app.route('/login', methods=['post', 'get'])
+@app.route('/login', methods=['post', 'get'])  # Логинизация
 def login():
     message = ''
-    if current_user.is_authenticated:
-        message = 'Вы уже авторизованы!'
+    if current_user.is_authenticated:  # Если авторизванный человек логинизируется
+        message = 'Ви вже авторизовані!'
         return render_template('main.html', message=message)
     form = LoginForm()
-    if form.validate_on_submit():
-        user = db.session.query(User).filter(User.email == form.email.data).first()
-        if user and user.check_password(form.password.data):
+    if form.validate_on_submit():  # Если нажал кнопку отправки формы
+        user = db.session.query(User).filter(User.email == form.email.data).first()  # Сверяемся с первым результатом в БД
+        if user and user.check_password(form.password.data):  # Проверяем хэш пароля на соответствие
             login_user(user, remember=form.remember.data)
-            return redirect('/')
+            return redirect('/')  # Если пароль совпал
         else:
-            flash(r'Невірний пароль або логін', 'error')
+            flash(r'Невірний пароль або логін', 'alert alert-warning')
             return redirect(url_for('login'))
     return render_template('login.html', form=form, message=message)
 
@@ -62,41 +62,42 @@ def admin():
     return render_template('/admin/admin.html')
 
 
-@app.route('/addpost', methods=['POST', 'GET'])
+@app.route('/addpost', methods=['POST', 'GET'])  # Добавление поста
 @login_required
 def addpost():
     form = Addpost()
     if request.method == 'POST':
         title = request.form['title']
         preview = request.form['preview']
-        text = form.body.data
+        text = form.body.data  # Ссылка на CKEditor
         name = request.form['autorsname']
         file = request.files['filepath']
         filename = 'default.jpeg'
-        if file and allowed_file(file.filename):
+        if file and allowed_file(file.filename):  # Загрузка фото-первью
             filename = file.filename
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         article = Article(title=title, preview=preview, text=text, autorsname=name, filepath=filename)
-        try:
+        try:  # Запись в БД
             db.session.add(article)
             db.session.commit()
-            msg = Message(f'Новий пост!', sender='ig.vasylenko2@gmail.com', recipients=['karonion4ik@gmail.com'])
+            msg = Message(f'Новий пост!', sender='ig.vasylenko2@gmail.com', recipients=['karonion4ik@gmail.com'])  # Оправка емеил сообщения администратору
             msg.body = f'Був доданий новий пост від користувача {name}, {datetime.utcnow().date()}'
             mail.send(msg)
-            return redirect('/')
+            flash("Пост відправлений на модерацію, ми опрацюємо його найближчим часом", 'alert alert-success')
+            return redirect(url_for('main'))
         except Exception as e:
             return e
     else:
         return render_template('/addpost.html', form=form)
 
 
-@app.route('/post/<int:id>')
+@app.route('/post/<int:id>')  # Пост подробно
 def get_exect_post(id):
     article_exect = Article.query.get(id)
     return render_template('post.html', article_exect=article_exect)
 
 
-@app.route('/logout/')
+@app.route('/logout/')  # Логаут
 @login_required
 def logout():
     logout_user()
@@ -106,7 +107,7 @@ def logout():
 @app.route('/register', methods=['post', 'get'])
 def register():
     message = ''
-    if current_user.is_authenticated:
+    if current_user.is_authenticated:  # Если человек уже авторизован, перенаправляем на главную
         return redirect('/')
     form = RegisterForm()
     if form.validate_on_submit():
@@ -114,32 +115,33 @@ def register():
         last_name = form.last_name.data
         email = form.email.data
         password = form.password
-        password_hash = generate_password_hash(password.data)
-        user = User(First_Name=first_name, Last_Name=last_name, email=email, password_hash=password_hash)
+        password_hash = generate_password_hash(password.data)  # Преобразуем пароль в хэш
+        user = User(First_Name=first_name, Last_Name=last_name, email=email, password_hash=password_hash)  # Записываем в БД
         try:
             db.session.add(user)
             db.session.commit()
-            msg = Message(f'Вдала реєстрація!', sender='ig.vasylenko2@gmail.com', recipients=[f'{email}'])
+            msg = Message(f'Вдала реєстрація!', sender='ig.vasylenko2@gmail.com', recipients=[f'{email}'])  # Отправка сообщения пользователю
             msg.html = render_template(r'Registration-email.html', login=email, password=password.data)
             mail.send(msg)
-            flash('Вдала реєстрація! Скористуйтесь логіном та паролем. Вони відправлені Вам на пошту.', 'success')
+            flash('Вдала реєстрація! Скористуйтесь логіном та паролем. Вони відправлені Вам на пошту.', 'alert alert-success')
             return redirect(url_for('login'))
         except Exception as e:
             return e
     return render_template('register.html', form=form)
 
 
-@app.route('/feedback', methods=['get', 'post'])
+@app.route('/feedback', methods=['get', 'post'])  # Обратная связь
 def send_feedback():
     form = FeedbackForm()
     if form.validate_on_submit():
         contact = form.contact.data
         text = form.text.data
-        feedback = Feedback_db(contact=contact, text=text)
+        feedback = Feedback_db(contact=contact, text=text)  # Записываем в БД
         try:
             db.session.add(feedback)
             db.session.commit()
-            return redirect('/')
+            flash("Дякуємо за зворотній звя'язок!", 'alert alert-success')  # Редирект на главную
+            return redirect(url_for('main'))
         except Exception as e:
             return e
     return render_template('/feedback.html', form=form)
